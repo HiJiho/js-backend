b2b1417d60d955eeb2b8e4abfc092b61e8fb90ed
김지호 <jiho001108@gmail.com>
2024-06-30 21:37:54 +0900

    Install express

--- chapter2/test_hello.js
+++ chapter2/test_hello.js
@@ -1,10 +1,10 @@
-import http from "k6/http";
+import http from 'k6/http';
 
 export const options = {
-  vus: 100,
-  duration: "10s",
+	vus: 200, // Virtual Users(가상 유저 수)
+	duration: '10s', // 테스트 진행 시간
 };
 
 export default function () {
-  http.get("http://localhost:8000");
+	http.get('http://localhost:8000');
 }
